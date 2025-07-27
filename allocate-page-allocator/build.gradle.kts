e302cf7ef0d45b66506b0216612dedda6bdf7c66
João Esperancinha <jofisaes@gmail.com>
2025-07-27 11:40:17 +0200

    More gradle updates

--- allocate-page-allocator/build.gradle.kts
+++ allocate-page-allocator/build.gradle.kts
@@ -1,18 +1,39 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
-group = "org.jesperancinha.knb"
+group = "me.user"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
 
-tasks.test {
-    useJUnitPlatform()
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+
+    sourceSets {
+        nativeMain.dependencies {
+            implementation(libs.kotlinxSerializationJson)
+        }
+    }
 }
---
+++ allocate-per-object/src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,20 @@
+package org.jesperancinha.knb
+
+import kotlin.time.measureTime
+
+class TestObject(val id: Int, val data: ByteArray)
+
+fun main() {
+    val objects = mutableListOf<TestObject>()
+
+
+    val duration =  measureTime {
+        repeat(100_000) {
+            objects.add(TestObject(it, ByteArray(1024)))
+        }
+    }
+
+    println("Allocated 100,000 objects in $duration ms")
+
+    readlnOrNull()
+}
